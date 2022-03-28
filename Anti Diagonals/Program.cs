026268412d340f71c42da660975302cbbb177dda
Mahdi Jafarkhani <Mahdi.Jafarkhani@Gmail.com>
2022-03-28 09:48:38 +0430

    Add Anti Diagnogals

---
+++ Anti Diagonals/Program.cs
@@ -0,0 +1,34 @@
+﻿List<List<int>> A = new List<List<int>>()
+{
+    new List<int>(){1,2,3},
+    new List<int>(){4,5,6},
+    new List<int>(){7,8,9}
+};
+
+var N = A.Count;
+
+List<List<int>> result = new List<List<int>>();
+
+for (int k = 0; k < 2 * N - 1; k++)
+{
+    List<int> current = new List<int>();
+
+    if (k < N)
+    {
+        for (int i = 0, j = k; i <= k && j > -1; i++, j--)
+        {
+            current.Add(A[i][j]);
+        }
+    }
+    else
+    {
+        for (int i = k - N + 1, j = N - 1; i < N && j > -1; i++, j--)
+        {
+            current.Add(A[i][j]);
+        }
+    }
+
+    result.Add(current);
+}
+
+return 1;
---
+++ Anti Diagonals/README.md
@@ -0,0 +1,3 @@
+Give a N*N square matrix, this code creates an array of its anti-diagonals.
+
+Full description : https://www.interviewbit.com/problems/anti-diagonals/
